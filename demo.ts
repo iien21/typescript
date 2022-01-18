90f714522a5dffe4c76efcf0e6c42ac2ff483af5
liuxinfeng <lxf_dream@qq.com>
2022-01-18 11:04:39 +0800

    181104

--- demo.ts
+++ demo.ts
@@ -1,5 +1,22 @@
 function fun0(name) {
   return name;
 }
-
 const con: string = null;
+
+
+function fun() {
+  let web: string = "world";
+  console.log(web);
+}
+fun();
+
+let count: number = 1;
+interface xiaojiejie {
+  name: string;
+  age: number;
+}
+const sisi: xiaojiejie = {
+  name: "小红",
+  age: 18,
+};
+console.log(sisi);
--- demo1.ts
+++
@@ -1,6 +0,0 @@
-function fun() {
-  let web: string = "world";
-  console.log(web);
-}
-
-fun();
